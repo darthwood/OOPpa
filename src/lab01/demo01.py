a7eac5daf04a77c9d34d02418e24a47163c16f7e
darthwood <dronasbis@yandex.ru>
2026-03-10 16:30:28 +0300

    changa

---
+++ src/lab01/demo01.py
@@ -0,0 +1,53 @@
+from model01 import *
+
+def run_full_test():
+    print(f"===$$$ ТЕСТИРОВАНИЕ БАНКОВСКОЙ СИСТЕМЫ СТАРТУЕТ $$$=== \n")
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 1 Валидация Кредита ===")
+    try:
+        bad_credit = Credit(1000, -5)
+    except ValueError as e:
+        print(f"Кредит отклонен: {e}")
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 2 Валидация Счета ===")
+    try:
+        bad_acc = BankAccount("123", 0)
+    except ValueError as e:
+        print(f"Аккаунт отклонен: {e}")
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 3 Валидация Клиента ===")
+    try:
+        valid_acc = BankAccount("ASS-123456", 1000)
+        bad_client = Client("", valid_acc)
+    except ValueError as e:
+        print(f"Клиент отклонен: {e}")
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 4 Минус на балансе ===")
+    acc = BankAccount("VIP-777-GOLDEN", 5000)
+    user = Client("Андрей Игоревич", acc)
+
+    user.spend_money(10000)
+    print(acc) #Должен показать что там теперь задолженность -5000 дораров
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 5 Попытка - не пытка ===")
+    user.take_loan(50000, 10.5) #Должен отказать, пока баланс минусовой
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 6 Депозит и история ===")
+    acc.change_balance(20000, "ВЗБЗДНОС   ") #выходим в плюс
+    dep = Deposit(10000, 5.0)
+    print(f"Создан объект вклада: {repr(dep)}")
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 7 Ледниковый период ===")
+    user.spend_money(70000) # уходим ниже -50000
+    print(acc)
+    user.spend_money(100) # Выводим что операция не прошла и счет заморожен
+
+    print("\n=== ВНИМАНИЕ! ТЕСТ 8 Сравниваем сущностей ===")
+    another_acc = BankAccount("VIP-777_GOLDEN", 0)
+    another_user = Client("Андрей Игоревич", another_acc)
+    print(f"Клиенты идентичны по номеру счета? {user == another_user}")
+
+
+if __name__ == "__main__":
+    run_full_test()
+
---
+++ src/lab01/model01.py
@@ -0,0 +1,122 @@
+class Transaction:
+    """Транзация"""
+    def __init__(self, t_type: str, amount: float):
+        self.__t_type = t_type
+        self.__amount = amount
+
+    def __repr__(self):
+        return f"[{self.__t_type}: {self.__amount:>8}$]"
+    
+
+
+class Credit:
+    """Кредит"""
+    def __init__(self, amount: float, rate: float):
+        self.__amount = self._validate(amount, "Сумма кредита")
+        self.__rate = self._validate(rate, "Ставка")
+        self.__is_paid = False
+
+    def _validate(self, val, name):
+        if val <= 0: raise ValueError(f"{name} Должна быть > 0")
+        return val
+    
+    def get_full_debt(self):
+        return self.__amount * (1 + self.__rate / 100)
+    
+    def __str__(self):
+        return f"Кредит на {self.__amount}$ (Ставка: {self.__rate}%)"
+    
+
+
+class Deposit:
+    """Депозит под процент""" 
+    def __init__(self, amount: float, rate: float):
+        self.__amount = amount
+        self.__rate = rate
+
+    def calculate_profit(self, months: int):
+        return self.__amount * (self.__rate / 100 / 12) * months
+
+    def __repr__(self):
+        return f"Депозит (сумма={self.__amount}, ставка={self.__rate}%)"
+
+
+
+class BankAccount:
+    """Банквский счет"""
+    bank_name = "NoMoney-NoHoney Group"
+
+    def __init__(self, acc_id: str, balance: float):
+        self.__acc_id = self._validate_id(acc_id)
+        self.__balance = balance
+        self.__history = []
+        self.__is_frozen = False
+
+    def _validate_id(self, val):
+        if len(val) < 5: raise ValueError("ID счета слишком короткий")
+        return val
+    
+    @property
+    def balance(self): return self.__balance
+
+    @property
+    def acc_id(self): return self.__acc_id
+
+    def change_balance(self, amount, op_type):
+        if self.__is_frozen:
+            print("Счет заморожен в морозилке")
+            return False
+
+        self.__balance += amount 
+        self.__history.append(Transaction(op_type, amount))
+
+        if self.__balance < -50000:
+            print("Слишком большой долг. Счет заморожен, продавай почку")
+            self.__is_frozen = True
+        return True
+    
+    def __str__(self):
+        status = "Заморожен" if self.__is_frozen else "Активен"
+        msg = f"Счет {self.__acc_id} | Баланс: {self.__balance}$ | {status}"
+        if self.__balance < 0:
+            msg == "!!! ВНИМАНИЕ: ДОЛЖНИК !!!"
+        return msg
+
+
+
+class Client:
+    """Клиент"""
+    def __init__(self, name: str, account: BankAccount):
+        self.__name = self._validate_name(name)
+        self.__account = account
+        self.__credits = []
+        self.__deposits = []
+
+    def _validate_name(self, name):
+        if not name: raise ValueError("Пустое значение!")
+        return name
+
+    def take_loan(self, amount: float, rate: float):
+        """Разрешение на кредит, проверка на задолженность"""
+        if self.__account.balance < 0:
+            print(f"{self.__name}, необходимо сначала погасить задолженность {self.__account.balance}$!")
+        else:
+            new_credit = Credit(amount, rate)
+            self.__credits.append(new_credit)
+            self.__account.change_balance(amount, "КРЕДИТ   ")
+            print(f"Кредит одобрен. деньги поступили на счет. Текущий баланс: {self.__account.balance}$")
+
+    def spend_money(self, amount: float):
+        """Уход баланса в минус"""
+        print(f"Попытка оплаты на {amount}$...")
+        if not self.__account.change_balance(-amount, "ОПЛАТА   "):
+            print(f"Транзакция отклонена! Счет заблокирован")
+
+    def show_status(self):
+        print(f"\n{self.__account}")
+        print(f"Долгов по кредитам: {len(self.__credits)}")
+
+    def __eq__(self, other):
+        if not isinstance(other, Client): return False
+        return self.__account.acc_id == other.__account.acc_id      
+
