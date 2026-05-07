fbe3b79d8fb2aed7d458dfddc31b3dd0cf6aadc4
Andrey <dronasbis@yandex.ru>
2026-05-07 16:41:34 +0300

    shshlo

--- src/lab04/interfaces.py
+++ src/lab04/interfaces.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 
-# Интерфейс Printable — обязывает классы уметь "показывать себя"
+
 class Printable(ABC):
     @abstractmethod
     def display(self) -> str:
         
         pass
 
-# Интерфейс Comparable — обязывает классы поддерживать сравнение
+
 class Comparable(ABC):
     @abstractmethod
     def compare_with(self, other) -> str:
