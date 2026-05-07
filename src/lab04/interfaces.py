4d809aacbe84b57e3c0330fecebbe604e3194cad
Andrey <dronasbis@yandex.ru>
2026-05-07 16:40:56 +0300

    step

--- src/lab04/interfaces.py
+++ src/lab04/interfaces.py
@@ -4,19 +4,13 @@ from abc import ABC, abstractmethod
 class Printable(ABC):
     @abstractmethod
     def display(self) -> str:
-        """
-        Абстрактный метод: кода нет, но каждый, кто 
-        реализует этот интерфейс, ОБЯЗАН написать свой display.
-        """
+        
         pass
 
 # Интерфейс Comparable — обязывает классы поддерживать сравнение
 class Comparable(ABC):
     @abstractmethod
     def compare_with(self, other) -> str:
-        """
-        Контракт на сравнение: заставляет реализовать 
-        логику сопоставления двух объектов.
-        """
+        
         pass
     
